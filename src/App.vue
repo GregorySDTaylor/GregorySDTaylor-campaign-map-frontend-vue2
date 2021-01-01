341026c8b01db94a2e8e37a615c713245bfc6769
Greg Taylor <GregorySDTaylor@gmail.com>
2021-01-01 15:00:40 -0500

    styled app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark> </v-app-bar>
+    <v-app-bar hide-on-scroll app dense>
+      <v-breadcrumbs :items="items" />
+    </v-app-bar>
     <v-main>
       <v-container fluid>
         <router-view />
@@ -12,5 +14,19 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      items: [
+        {
+          to: "/",
+          text: "todo",
+        },
+        {
+          to: "/",
+          text: "breadcrumbs",
+        },
+      ],
+    };
+  },
 };
 </script>
